f780a8f5da5ce6c5201c90c875a83b6194d78a86
Vanessa Toro <vanetoro84@gmail.com>
2018-08-09 19:26:34 +0000

    Done.

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ $(document).ready(function (){
 
 const api = 'https://api.github.com/users/'
 
-function getRepositories(user){
+function getRepositories(){
   const req = new XMLHttpRequest()
   req.addEventListener('load', showRepositories);
   let username = document.getElementById('searchTerms').value
-  req.open('GET', api + username )
+  req.open('GET', api + username +'/repos' )
   req.send()
-
 }
 
 
 function showRepositories(){
-  debugger
+  const repos = JSON.parse(this.responseText)
+  var repoList = repos.map( r=> `<ul>${ ('<li>'+ r.name + '-' + r.description + '-' + r.html_url + '</li>')} </ul>`)
+  document.getElementById('results').innerHTML = repoList
+
 }
