3d0e713e0e17f6c5d0554bfb22ad9fdc19462130
Vanessa Toro <vanetoro84@gmail.com>
2018-08-09 22:08:06 -0400

    Done.

--- index.js
+++ index.js
@@ -2,24 +2,40 @@ $(document).ready(function (){
 
 });
 
-const api = 'https://api.github.com/users/'
+var renderSearchResults = (data) => data.items.map( result => renderSearchResult(result))
 
-function getRepositories(){
-  const req = new XMLHttpRequest()
-  req.addEventListener('load', showRepositories);
-  let username = document.getElementById('searchTerms').value
-  req.open('GET', api + username +'/repos' )
-  req.send()
+var searchRepositories = () => {
+  let searchTerms = $('#searchTerms').value()
+  $.get('https://api.github.com/search/repositories?q=${searchTerms}', data =>{
+    $('#results').html(renderSearchResults(data))
+  })
+
 }
 
 
 function showRepositories(){
   const repos = JSON.parse(this.responseText)
-  const repoName = repos.name
-  const repoDescription = repos.description
-  const repoUrl = repos.html_url
-  const avatar = repos.owner.avatar_url
-  debugger
-  var repoList = repos.map( r=> `<ul>${ ('<li>'+ r.name + '-' + r.description + '-' + r.html_url + '</li>')} </ul>`)
+  var repoList = '<ul>'
+  repoList = repos.map( function(r){
+    var description = `Description: ${r.description}  <br>`
+    var avatar = `<img width= "50" src='${r.owner.avatar_url}'> <br>`
+    var name = `Name: ${r.name}<br>`
+    var owner = `Owner: ${r.owner.login}<br>`
+    var url = `Url: ${r.html_url}`
+    var commits = `<a href='#' data-repo="${name}" onclick='getCommits(${r.commits_url})'>Get Commits</a>`
+      return `<li>${avatar}${name}${description}${owner}${url}${commits}</li>`
+})
+repoList+= '</ul>'
   document.getElementById('results').innerHTML = repoList
+}
+
+function getCommits(el){
+  const req = new XMLHttpRequest()
+  req.addEventListener('load', showcommits);
+  req.open('GET', el)
+}
+
+
+function showcommits(){
+
 }
