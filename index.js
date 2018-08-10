7cf9501c694c8e6b9b14998c308bcd07c9c42355
Vanessa Toro <vanetoro84@gmail.com>
2018-08-10 17:44:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,22 +5,35 @@ $(document).ready(function (){
 
 function searchRepositories() {
   let searchTerms = $('#searchTerms').val()
-  fetch(`https://api.github.com/search/repositories?q=${searchTerms}`)
-  .then(function(response){
-    return response.json();
-  })
-  .then(function(data){
+$.get(`https://api.github.com/search/repositories?q=${searchTerms}`, function(data){
+  console.log(data)
   var repo = data.items[0]
   var img = `<img height="50"src="${repo.owner.avatar_url}"><br>`
   let login = `Login: ${repo.owner.login}<br>`
   let search = `<h2>Search for: ${searchTerms}</h2>`
-  var displayData = `${img} ${login} ${repo.name} - ${repo.description} - ${repo.html_url}<br> <a data-owner="${repo.owner.login}" data-name="${repo.name}"href="#" onclick="showCommits(this)">Show Commits</a>`
+  var displayData = `${search}${img} ${login} ${repo.name} - ${repo.description} - ${repo.html_url}<br> <a data-owner="${repo.owner.login}" data-name="${repo.name}"href="#" onclick="showCommits(this)">Show Commits</a>`
     $('#results').html(`${displayData}`)
-  })
-  .catch(function(error){
-    displayError()
-  })
+})
 }
+
+// function searchRepositories() {
+//   let searchTerms = $('#searchTerms').val()
+//   fetch(`https://api.github.com/search/repositories?q=${searchTerms}`)
+//   .then(function(response){
+//     return response.json();
+//   })
+//   .then(function(data){
+//   var repo = data.items[0]
+//   var img = `<img height="50"src="${repo.owner.avatar_url}"><br>`
+//   let login = `Login: ${repo.owner.login}<br>`
+//   let search = `<h2>Search for: ${searchTerms}</h2>`
+//   var displayData = `${search}${img} ${login} ${repo.name} - ${repo.description} - ${repo.html_url}<br> <a data-owner="${repo.owner.login}" data-name="${repo.name}"href="#" onclick="showCommits(this)">Show Commits</a>`
+//     $('#results').html(`${displayData}`)
+//   })
+//   .catch(function(error){
+//     displayError()
+//   })
+// }
 
   function showCommits(el){
     let repo = el.dataset.name
