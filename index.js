4a4443fc265a097b94668299f0839c89473711ed
Vanessa Toro <vanetoro84@gmail.com>
2018-08-10 17:39:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ function searchRepositories() {
   var repo = data.items[0]
   var img = `<img height="50"src="${repo.owner.avatar_url}"><br>`
   let login = `Login: ${repo.owner.login}<br>`
-  let search = `<h2>Search for: ${searchTerms}</h2>`
-  var displayData = `${search}${img} ${login} ${repo.name} - ${repo.description} - ${repo.html_url}<br> <a data-owner="${repo.owner.login}" data-name="${repo.name}"href="#" onclick="showCommits(this)">Show Commits</a>`
+  // let search = `<h2>Search for: ${searchTerms}</h2>`
+  var displayData = `${img} ${login} ${repo.name} - ${repo.description} - ${repo.html_url}<br> <a data-owner="${repo.owner.login}" data-name="${repo.name}"href="#" onclick="showCommits(this)">Show Commits</a>`
     $('#results').html(`${displayData}`)
   })
   .catch(function(error){
